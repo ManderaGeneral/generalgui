d0badf04898efe414196526d29dc325ea6b68992
Mandera <krizoorn@hotmail.com>
2020-06-03 23:05:05 +0200

    Added sort reset to spreadsheet. Started on loadTSV, working but need to add option for headers.

--- generalgui/pages/spreadsheet.py
+++ generalgui/pages/spreadsheet.py
@@ -28,12 +28,12 @@ class Spreadsheet(Page):
             self.columnKeysPageContainer = Page(self, pack=True, fill="x")
             self.columnKeysFillerLeft = Frame(self.columnKeysPageContainer, side="left", fill="y")
             self.columnKeysGrid = Grid(self.columnKeysPageContainer, height=30, pack=True, side="left", scrollable=True, mouseScroll=False, fill="x", expand=True)
-            self.columnKeysGrid.menu("Column", Remove_column=self.removeColumn)
+            self.columnKeysGrid.menu("Column", Remove_column=lambda: self.drop(columns=True))
 
         if self.rowKeys:
             self.rowKeysPageContainer = Page(self, pack=True, width=0, side="left", fill="y", pady=1)  # Pady=1 for frames in row 0 being 1 pixel high
             self.rowKeysGrid = Grid(self.rowKeysPageContainer, pack=True, side="top", width=100, scrollable=True, mouseScroll=False, fill="both", expand=True)
-            self.rowKeysGrid.menu("Row", Remove_row=self.removeRow)
+            self.rowKeysGrid.menu("Row", Remove_row=lambda: self.drop(rows=True))
 
         self.mainGrid = Grid(self, scrollable=True, hsb=cellHSB, vsb=cellVSB, pack=True, fill="both", expand=True)
 
@@ -55,53 +55,40 @@ class Spreadsheet(Page):
 
         self.pack()
 
-        self.menu("Spreadsheet", Save_As_TSV=self.saveAsTSV)
+        self.menu("Spreadsheet", Save_as_tsv=self.saveAsTSV, Load_tsv_file=self.loadTSV, Reset_sort=self.resetSort)
 
         # self.app.createBind("<Button-1>", lambda event: print(event), name="Spreadsheet")
 
-    def saveAsTSV(self):
-        path = Path(filedialog.askdirectory()).addPath("dataframe.tsv")
-        File.write(path, self.dataFrame)
+    def loadTSV(self):
+        """
+        Load a tsv file
 
-    def drop(self, value, axis):
-        self.dataFrame.drop(value, axis=axis, inplace=True)
+        HERE ** Working, but headers can be messed up
+        """
+        filetypes = [("Open a tsv file", ".tsv")]
+        path = filedialog.askopenfilename(title="Select spreadsheet", filetypes=filetypes)
+        if path:
+            read = File.read(path)
+            if read is not None:
+                self.dataFrame = read
+                self.loadDataFrame()
+
+    def saveAsTSV(self):
+        """Save current Data Frame as a tsv file, asks user where to put file."""
+        filetypes = [("Save spreadsheet as tsv", ".tsv")]
+        path = filedialog.asksaveasfilename(filetypes=filetypes, defaultextension=".tsv", title="Save spreadsheet", initialfile="Spreadsheet")
+        if path:
+            File.write(path, self.dataFrame, overwrite=True)
+
+    def resetSort(self):
+        """Reset the sorting of dataframe"""
+        # self.dataFrame.sort_index(inplace=True)
+        self.dataFrame = self.dataFrame.reindex(sorted(self.dataFrame.columns), axis=1)
+        self.dataFrame = self.dataFrame.reindex(sorted(self.dataFrame.index), axis=0)
         self.loadDataFrame()
 
-    def removeColumn(self):
-        self.drop(self.app.menuTargetElement.getValue(), "columns")
-
-    def removeRow(self):
-        self.drop(self.app.menuTargetElement.getValue(), "rows")
-
-    cellConfig = {"padx": 5, "relief": "groove", "bg": "gray85"}
-    def loadDataFrame(self, df=None):
-        """
-        Update cells to represent current dataFrame
-        """
-        if df is not None:
-            self.dataFrame = df
-        df = self.dataFrame
-
-        if self.columnKeys:
-            size = Vec2(len(df.columns), 1)
-            self.columnKeysGrid.fillGrid(Frame, Vec2(0, 0), size, height=1)
-            self.columnKeysGrid.fillGrid(Label, Vec2(0, 1), size, values=df.columns, removeExcess=True, onClick=lambda e: self.sortByColumn(e), **self.cellConfig)
-            self.mainGrid.fillGrid(Frame, Vec2(0, 0), size, height=1)
-
-        if self.rowKeys:
-            self.rowKeysGrid.fillGrid(Label, Vec2(0, 0), Vec2(1, len(df.index)), values=df.index, removeExcess=True, onClick=lambda e: self.sortByColumn(e), **self.cellConfig)
-
-        values = []
-        for row in df.itertuples(index=False):
-            values.extend(row)
-        self.mainGrid.fillGrid(Label, Vec2(0, 1), Vec2(df.shape[1], df.shape[0]), values=values, removeExcess=True, **self.cellConfig)
-
-        self._syncColumnKeysWidth()
-        self._syncRowKeysWidth()
-        self.app.widget.update()
-        self._syncKeysScroll()
-
-    def sortByColumn(self, event):
+    def sort(self, event):
+        """Sort a row or column by values, TypeError is silenced if there are mixed values."""
         element = event.widget.element
         rowPressed = 0 if element.parentPage == getattr(self, "columnKeysGrid", None) else 1
         value = element.getValue()
@@ -127,6 +114,57 @@ class Spreadsheet(Page):
             return
 
         self.loadDataFrame()
+
+    def drop(self, value=None, columns=False, rows=False):
+        """
+        Remove a column or row
+
+        :param value: Index of either a column or a row, leave as None to use menuTarget
+        :param columns:
+        :param rows:
+        :raises AttributeError: If columns and rows are False
+        :raises ValueError: If value and menuTarget is None
+        """
+        if not columns and not rows:
+            raise AttributeError("Columns or rows has to be set to True")
+
+        if value is None:
+            if self.app.menuTargetElement is None:
+                raise ValueError("value is None and app.menuTargetElement is None")
+
+            value = self.app.menuTargetElement.getValue()
+
+        axis = "columns" if columns else "rows"
+        self.dataFrame.drop(value, axis=axis, inplace=True)
+        self.loadDataFrame()
+
+    cellConfig = {"padx": 5, "relief": "groove", "bg": "gray85"}
+    def loadDataFrame(self, df=None):
+        """
+        Update cells to represent current dataFrame
+        """
+        if df is not None:
+            self.dataFrame = df
+        df = self.dataFrame
+
+        if self.columnKeys:
+            size = Vec2(len(df.columns), 1)
+            self.columnKeysGrid.fillGrid(Frame, Vec2(0, 0), size, height=1)
+            self.columnKeysGrid.fillGrid(Label, Vec2(0, 1), size, values=df.columns, removeExcess=True, onClick=lambda e: self.sort(e), **self.cellConfig)
+            self.mainGrid.fillGrid(Frame, Vec2(0, 0), size, height=1)
+
+        if self.rowKeys:
+            self.rowKeysGrid.fillGrid(Label, Vec2(0, 0), Vec2(1, len(df.index)), values=df.index, removeExcess=True, onClick=lambda e: self.sort(e), **self.cellConfig)
+
+        values = []
+        for row in df.itertuples(index=False):
+            values.extend(row)
+        self.mainGrid.fillGrid(Label, Vec2(0, 1), Vec2(df.shape[1], df.shape[0]), values=values, removeExcess=True, **self.cellConfig)
+
+        self._syncColumnKeysWidth()
+        self._syncRowKeysWidth()
+        self.app.widget.update()
+        self._syncKeysScroll()
 
     def _syncKeysScroll(self, _=None):
         if self.columnKeys:
