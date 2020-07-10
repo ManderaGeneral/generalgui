54c0534b3990fcf8064fded084a953b020674778
Mandera <krizoorn@hotmail.com>
2020-07-10 18:27:53 +0200

    Added all getFirstPatternPos parameters to ElementList

--- generalgui/pages/elementlist.py
+++ generalgui/pages/elementlist.py
@@ -2,13 +2,28 @@
 
 from generalgui import Grid
 
+from generalvector import Vec2
+
 class ElementList(Grid):
     """
     Controls any amount of elements and stacks them nicely.
     Elements are added to grid automatically.
     """
-    def __init__(self, parentPage=None, maxFirstSteps=5, **parameters):
+    def __init__(self, parentPage=None, startPos=Vec2(0), firstStep=Vec2(0, 1), secondStep=Vec2(1, 0), maxFirstSteps=5, **parameters):
+        """
+
+        :param generalgui.Page parentPage:
+        :param Vec2 startPos:
+        :param Vec2 firstStep:
+        :param Vec2 secondStep:
+        :param int maxFirstSteps:
+        :param parameters:
+        """
         super().__init__(parentPage=parentPage, **parameters)
+
+        self.startPos = startPos
+        self.firstStep = firstStep
+        self.secondStep = secondStep
         self.maxFirstSteps = maxFirstSteps
         self.pack()
 
@@ -22,7 +37,7 @@ class ElementList(Grid):
         if element == self.frame:
             super().packPart(element)
         else:
-            element.grid(self.getFirstPatternPos(maxFirstSteps=self.maxFirstSteps))
+            element.grid(self.getFirstPatternPos(startPos=self.startPos, firstStep=self.firstStep, secondStep=self.secondStep, maxFirstSteps=self.maxFirstSteps))
 
 
 
