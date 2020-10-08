64ad1f1994a6a1a60a2d276dbb745f3a75758a1f
Mandera <krizoorn@hotmail.com>
2020-10-08 08:55:19 +0200

    Remove test

--- randomtesting.py
+++ randomtesting.py
@@ -1,34 +1,30 @@
 """Random testing"""
 
-from generallibrary.time import sleep
-
-from generalgui import App, Grid, Page, Button, Label, OptionMenu, Checkbutton, Entry, LabelCheckbutton, LabelEntry, Spreadsheet
-
-from generalvector import Vec2
-
-import pandas as pd
-
-import inspect
-
-
+# from generallibrary.time import sleep
+#
+# from generalgui import App, Grid, Page, Button, Label, OptionMenu, Checkbutton, Entry, LabelCheckbutton, LabelEntry, Spreadsheet
+#
+# from generalvector import Vec2
+#
+# import pandas as pd
+#
+# import inspect
 
 
-
-
-
-page = Page(App(), resizeable=True)
-label = Label(page, "hello")
-
-label.show()
-
-
-
-
+# page = Page(App(), resizeable=True)
+# label = Label(page, "hello")
+#
+# label.show(
 
 
 
 # Label(page, "hello\nthere", hideMultiline=True).show()
 
 
+from tkinter import Tk, Label
 
-
+root = Tk()
+label = Label(root)
+label.pack()
+root.bind("<Motion>", lambda event: label.configure(text=f"{event.x}, {event.y}"))
+root.mainloop()
