7ceecf21fe82f760c2d74b4425e7600ba414540b
Mandera <krizoorn@hotmail.com>
2020-11-01 12:16:07 +0100

    Ideas for tkinter config

--- randomtesting.py
+++ randomtesting.py
@@ -4,14 +4,74 @@ import tkinter as tk
 from generallibrary import initBases, getBaseClasses
 import pickle
 
-from generalgui import Label
+# from generalgui import Label
+# Label("hello")
+
+print(...)
+
+
+# HERE ** Plan was to store all args as attributes. BUT: they should be protected. I dont want to make a property for each attribute.
+# WANT: Easy autocompletion for each arg
+# WANT: Ability to configure multiple
+# WANT: Automatic updating when changing a value
+# WANT: Allowing adding to args lower in hierarchy (i.e. value for Label)
+
+# DONT: Update after changing one of multiple values, only once
+# DONT: Rely on manual updating as it's prone for de-sync issues
+
+# IDEA: Remove config method, make them all attributes,
+# IDEA: Convert old Styler to a config class for generallibrary, use that here
+# Give combined lists to Config class, then add code for our purpose
 
 
 
 
-
-
-
-
-
-
+# Create_Construct
+    # @deco_group_top
+    # def get_shown(self, propagate=True):
+    #     """ Get whether this part and all its parents aren't is_hidden.
+    #
+    #         :param generalgui.MethodGrouper self:
+    #         :param propagate: """
+    #     if not propagate:
+    #         return self._shown
+    #
+    #     for part in self.parents(include_self=True):
+    #         if not part.get_shown(propagate=False):
+    #             return False
+    #     return True
+    #
+    # @deco_group_top
+    # def set_shown(self, shown):
+    #     """ Show this part.
+    #         If this part doesn't have a widget then it's created.
+    #         If widget is hidden then it's re-packed.
+    #
+    #
+    #         :param generalgui.MethodGrouper self:
+    #         :param shown: """
+    #     if shown:
+    #         self._shown = False
+    #         if self.destroy_when_hidden:
+    #             return self.remove()
+    #         else:
+    #             self.widget.hide()
+    #     else:
+    #         pass
+    #
+    # @deco_group_top
+    # def get_removed(self):
+    #     """ Remove this part from it's parent's children and destroys widget.
+    #
+    #         :param generalgui.MethodGrouper self: """
+    #
+    # @deco_group_top
+    # def set_removed(self):
+    #     """ Remove this part from it's parent's children and destroys widget.
+    #
+    #         :param generalgui.MethodGrouper self: """
+    #     if self.parent():
+    #         self.parent().children.remove(self)
+    #
+    #     self._parent = None
+    #     self._destroy()
